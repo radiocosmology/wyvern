caa04199c789a75217b0a78e432cef102b9433c3
Liam Gray <lgray@phas.ubc.ca>
2026-07-20 17:41:01 -0700

    style: rename python-specific mods

--- src/pylinear.rs
+++ src/pylinear.rs
@@ -2,44 +2,13 @@
 use num_complex::Complex;
 use num_traits::Float;
 use numpy::{
-    Element, PyArray2, PyArrayDescr, PyArrayDescrMethods, PyArrayMethods, PyReadonlyArray1,
-    PyReadonlyArray2, PyUntypedArray, PyUntypedArrayMethods, dtype,
+    Element, PyArray2, PyArrayDescrMethods, PyArrayMethods, PyReadonlyArray1, PyReadonlyArray2,
+    PyUntypedArray, PyUntypedArrayMethods, dtype,
 };
-use pyo3::exceptions::{PyTypeError, PyValueError};
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 
-/// Validate input dimension and return a python-compatible error
-fn require_ndim(arr: &Bound<'_, PyUntypedArray>, name: &str, expected_dim: usize) -> PyResult<()> {
-    // NB: without this check, an invalid dimension just returns
-    // "`ndarray` does not have type ndarray" when called from
-    // Python, which isn't very helpful
-    if arr.ndim() != expected_dim {
-        return Err(PyValueError::new_err(format!(
-            "'{name}' must be a {expected_dim}D array; got {:?}D with shape {:?}",
-            arr.ndim(),
-            arr.shape()
-        )));
-    }
-    Ok(())
-}
-
-/// Validate dtype and return a python-compatible error
-fn require_dtype(
-    arr: &Bound<'_, PyUntypedArray>,
-    name: &str,
-    expected_dtype: &Bound<'_, PyArrayDescr>,
-) -> PyResult<()> {
-    // NB: without this check, an invalid dtype just returns
-    // "`ndarray` does not have type ndarray" when called from
-    // Python, which isn't very helpful
-    if !arr.dtype().is_equiv_to(expected_dtype) {
-        return Err(PyValueError::new_err(format!(
-            "'{name}' must have type {expected_dtype}; got {:?}",
-            arr.dtype(),
-        )));
-    }
-    Ok(())
-}
+use crate::pyutils::{require_dtype, require_ndim};
 
 /// Separate methods for interpolating real and complex, weighted
 /// and unweighted
