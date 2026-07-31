0bbeeb08ce17bbc2d713cf2cf733bf52990dff2a
Liam Gray <lgray@phas.ubc.ca>
2026-07-31 10:30:11 -0700

    refactor(core): split plans into their own modules

--- src/interp/mod.rs
+++ src/interp/mod.rs
@@ -1,8 +1,14 @@
+//! Fundamental implementation of fast interpolators
+mod kernel_plan;
+mod linear_plan;
 mod ops;
 mod plan;
 
+// re-export
+pub use kernel_plan::KernelPlan;
+pub use linear_plan::LinearPlan;
 pub use ops::{
     interp_last_ax_complex, interp_last_ax_complex_weighted, interp_last_ax_real,
     interp_last_ax_real_weighted,
 };
-pub use plan::{InterpolationPlan, KernelPlan, LinearPlan};
+pub use plan::InterpolationPlan;
