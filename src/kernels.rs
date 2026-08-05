9d89c7cff5b3be5e6199dfc248546eee56ade118
Liam Gray <lgray@phas.ubc.ca>
2026-08-05 16:14:05 -0700

    fix(kernels): fix sinc function at x = 0

--- src/kernels.rs
+++ src/kernels.rs
@@ -3,10 +3,20 @@
 /// Lanczos kernel
 #[inline]
 pub fn lanczos_kernel(x: f64, a: f64) -> f64 {
-    if x == 0.0 || x.abs() >= a {
+    if x.abs() >= a {
         0.0
     } else {
+        sinc(x) * sinc(x / a)
+    }
+}
+
+/// Simple `sinc` implementation
+#[inline]
+fn sinc(x: f64) -> f64 {
+    if x == 0.0 {
+        1.0
+    } else {
         let px = std::f64::consts::PI * x;
-        a * px.sin() * (px / a).sin() / (px * px)
+        px.sin() / px
     }
 }
