0ec9820e3a8a19fbf4e6f35bbd20335eb1fcc291
Liam Gray <lgray@phas.ubc.ca>
2026-08-07 12:24:38 -0700

    feat(name): rename project

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,7 @@ mod types;
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 /// Rust-based fast interpolation.
-#[pymodule]
-fn interprs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn init_interpolate(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(
         interpolate::python::lanczos::interpolate_lanczos,
         m
@@ -28,6 +27,23 @@ fn interprs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
         interpolate::python::linear::interpolate_linear_weighted,
         m
     )?)?;
+
+    Ok(())
+}
+
+#[pymodule]
+fn wyvern(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    let py = m.py();
+
+    // submodule container
+    let interpolate_submodule = PyModule::new(py, "interpolate")?;
+    init_interpolate(&interpolate_submodule)?;
+    // add to parent module
+    m.add_submodule(&interpolate_submodule)?;
+
+    // add to sys.modules
+    let sys_modules = py.import("sys")?.getattr("modules")?;
+    sys_modules.set_item("wyvern.interpolate", &interpolate_submodule)?;
 
     Ok(())
 }
