b301e675402defdbabeaa32e36ba5d52cb9c9f23
Liam Gray <lgray@phas.ubc.ca>
2026-08-14 17:46:22 -0700

    refactor: move ops code into 'ParallelInterpolator' object

--- wyvern/src/interpolate/mod.rs
+++ wyvern/src/interpolate/mod.rs
@@ -1,15 +1,10 @@
 //! Fundamental implementation of fast interpolators
 mod helpers;
 mod interpolator;
-mod kernel_plan;
-mod linear_plan;
-mod ops;
+mod kernel_interpolator;
+mod linear_interpolator;
 
 // re-export
-pub use interpolator::{InterpolationPlan, Interpolator, IntoInterpolator};
-pub use kernel_plan::{DynamicKernelInterpolator, KernelInterpolator};
-pub use linear_plan::LinearInterpolator;
-pub use ops::{
-    interp_last_ax_complex, interp_last_ax_complex_weighted, interp_last_ax_real,
-    interp_last_ax_real_weighted,
-};
+pub use interpolator::{InterpolationPlan, Interpolator, IntoInterpolator, ParallelInterpolator};
+pub use kernel_interpolator::{DynamicKernelInterpolator, KernelInterpolator};
+pub use linear_interpolator::LinearInterpolator;
