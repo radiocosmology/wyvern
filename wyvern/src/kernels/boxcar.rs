9fbec41f2f6ff432fc7196bd4b59559eb9b5d64d
Liam Gray <lgray@phas.ubc.ca>
2026-08-19 14:36:12 -0700

    feat(kernel): add a simple boxcar kernel

---
+++ wyvern/src/kernels/boxcar.rs
@@ -0,0 +1,49 @@
+//! Boxcar kernel
+use super::traits::Kernel;
+
+#[derive(Debug)]
+pub struct BoxKernel {
+    /// Number of taps
+    ntaps: usize,
+    /// Width parameter
+    a: f64,
+}
+
+impl Kernel for BoxKernel {
+    #[allow(
+        clippy::integer_division,
+        clippy::cast_precision_loss,
+        reason = "truncated division is desired"
+    )]
+    fn build(ntaps: usize) -> Self {
+        let a = (ntaps / 2) as f64;
+
+        Self { ntaps, a }
+    }
+
+    #[inline]
+    fn evaluate(&self, x: f64) -> f64 {
+        if x.abs() >= self.a { 0.0 } else { 1.0 }
+    }
+
+    #[inline]
+    fn half_width(&self) -> f64 {
+        self.a
+    }
+
+    #[inline]
+    fn ntaps(&self) -> usize {
+        self.ntaps
+    }
+
+    #[allow(
+        clippy::integer_division,
+        clippy::cast_precision_loss,
+        reason = "truncated division is desired"
+    )]
+    #[inline]
+    fn set_ntaps(&mut self, ntaps: usize) {
+        self.ntaps = ntaps;
+        self.a = (ntaps / 2) as f64;
+    }
+}
--- wyvern/src/kernels/mod.rs
+++ wyvern/src/kernels/mod.rs
@@ -1,7 +1,9 @@
 //! Kernels for interpolation, resampling, etc...
+mod boxcar;
 mod kaiserbessel;
 mod lanczos;
 pub mod traits;
 
+pub use boxcar::BoxKernel;
 pub use kaiserbessel::KaiserBesselKernel;
 pub use lanczos::LanczosKernel;
