f6d7134013d18bec341d231efe7523132f5daf93
Liam Gray <lgray@phas.ubc.ca>
2026-08-24 12:32:21 -0700

    bench: add benchmarks for 2D parallel interpolation

--- wyvern/benches/interpolate/main.rs
+++ wyvern/benches/interpolate/main.rs
@@ -2,7 +2,12 @@
 mod common;
 mod kernel;
 mod linear;
+mod parallel;
 
 use criterion::criterion_main;
 
-criterion_main!(linear::linear_benches, kernel::lanczos_benches);
+criterion_main!(
+    linear::linear_benches,
+    kernel::lanczos_benches,
+    parallel::parallel_benches
+);
