4f19415878d04e2f8061ab8b56dee9e196307179
Liam Gray <lgray@phas.ubc.ca>
2026-08-25 16:09:15 -0700

    bench: add standard complex benchmark for kernel interpolator

--- wyvern/benches/interpolate/linear.rs
+++ wyvern/benches/interpolate/linear.rs
@@ -45,7 +45,7 @@ fn bench_base_complex(
     interpolator.interp_row(y_in, y_out);
 }
 
-fn bench_masked(
+fn bench_masked_real(
     interpolator: &interpolate::LinearInterpolator,
     y_in: &[f64],
     mask_in: &mut [f64],
@@ -55,7 +55,17 @@ fn bench_masked(
     interpolator.interp_row_masked(y_in, mask_in, y_out);
 }
 
-fn bench_with_variance(
+fn bench_masked_complex(
+    interpolator: &interpolate::LinearInterpolator,
+    y_in: &[Complex<f64>],
+    mask_in: &mut [f64],
+    y_out: &mut [Complex<f64>],
+) {
+    let interpolator: &dyn Interpolator<Complex<f64>> = interpolator.as_interpolator();
+    interpolator.interp_row_masked(y_in, mask_in, y_out);
+}
+
+fn bench_with_variance_real(
     interpolator: &interpolate::LinearInterpolator,
     y_in: &[f64],
     weight_in: &[f64],
@@ -75,6 +85,25 @@ fn bench_with_variance(
     );
 }
 
+fn bench_with_variance_complex(
+    interpolator: &interpolate::LinearInterpolator,
+    y_in: &[Complex<f64>],
+    weight_in: &[f64],
+    var_scratch: &mut [f64],
+    mask_scratch: &mut [f64],
+    y_out: &mut [Complex<f64>],
+    weight_out: &mut [f64],
+) {
+    let interpolator: &dyn Interpolator<Complex<f64>> = interpolator.as_interpolator();
+    interpolator.interp_row_with_variance(
+        y_in,
+        weight_in,
+        var_scratch,
+        mask_scratch,
+        y_out,
+        weight_out,
+    );
+}
 fn run_benchmarks(c: &mut Criterion) {
     let n_in: usize = 9200;
     let n_out: usize = 16382;
@@ -109,9 +138,9 @@ fn run_benchmarks(c: &mut Criterion) {
             );
         });
     });
-    group.bench_function("interpolate_masked", |b| {
+    group.bench_function("interpolate_masked_real", |b| {
         b.iter(|| {
-            bench_masked(
+            bench_masked_real(
                 black_box(&interpolator),
                 black_box(y_in.as_slice().unwrap()),
                 black_box(mask.as_mut_slice()),
@@ -119,15 +148,38 @@ fn run_benchmarks(c: &mut Criterion) {
             );
         });
     });
-    group.bench_function("interpolate_with_variance", |b| {
+    group.bench_function("interpolate_masked_complex", |b| {
         b.iter(|| {
-            bench_with_variance(
+            bench_masked_complex(
+                black_box(&interpolator),
+                black_box(y_in_c.as_slice().unwrap()),
+                black_box(mask.as_mut_slice()),
+                black_box(y_out_c.as_slice_mut().unwrap()),
+            );
+        });
+    });
+    group.bench_function("interpolate_with_variance_real", |b| {
+        b.iter(|| {
+            bench_with_variance_real(
                 black_box(&interpolator),
                 black_box(y_in.as_slice().unwrap()),
                 black_box(weight_in.as_slice().unwrap()),
                 black_box(var.as_mut_slice()),
                 black_box(mask.as_mut_slice()),
                 black_box(y_out.as_slice_mut().unwrap()),
+                black_box(weight_out.as_slice_mut().unwrap()),
+            );
+        });
+    });
+    group.bench_function("interpolate_with_variance_complex", |b| {
+        b.iter(|| {
+            bench_with_variance_complex(
+                black_box(&interpolator),
+                black_box(y_in_c.as_slice().unwrap()),
+                black_box(weight_in.as_slice().unwrap()),
+                black_box(var.as_mut_slice()),
+                black_box(mask.as_mut_slice()),
+                black_box(y_out_c.as_slice_mut().unwrap()),
                 black_box(weight_out.as_slice_mut().unwrap()),
             );
         });
